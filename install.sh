d56f45ca642ecd08beb9fa2854333e02850cf890
omar.jamil <o.jamil@exeter.ac.uk>
2021-09-16 12:48:26 +0100

    typo fix in install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ mv .bashrc .bash_profile .vimrc .vim .bash config_backup
 # create softlinks 
 ln -s ~/dotfiles/bash ~/.bash
 ln -s ~/dotfiles/bashrc ~/.bashrc
-ln -s ~/dotfiles/bash_prfile ~/.bash_profile
+ln -s ~/dotfiles/bash_profile ~/.bash_profile
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/shell ~/.shell
@@ -16,6 +16,4 @@ ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/nvim ~/.config/nvim
 
 echo "To install neovim"
-echo "curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage"
-echo "chmod u+x nvim.appimage"
-echo "./nvim.appimage"
+echo "curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage && chmod u+x nvim.appimage"
